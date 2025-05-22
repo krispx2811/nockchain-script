1701e8c18684709be6b5d590564d7ecf739ad539
Kareem <38662249+krispx2811@users.noreply.github.com>
2025-05-22 15:04:41 +0400

    Update install_nockapp.sh

--- install_nockapp.sh
+++ install_nockapp.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# Only thing you need to change:
+# ← ONLY change this:
 PUBKEY="3KqWPYrkZxM2FjiJ6v1fdff87j9EqARRJifZp67u5jSTvFhqPWkcj37dkUHAMhfDUT5NUtC7Fzjymd6Kd377f4JNzf1ZEZb82wwERPLMw4KriHrrVnJumcWr4A7J2Yq1qtqQ"
 
-# kill old session if it exists
+# remove old session
 tmux kill-session -t nock-miner 2>/dev/null || true
 
-# show the binary
+# verify binary exists
 ls ~/nockchain/target/release/nockchain
 
-# start miner in tmux
-tmux new-session -d -s nock-miner "cd ~/nockchain && ./target/release/nockchain --mining-pubkey $PUBKEY --mine"
+# launch in tmux
+tmux new-session -d -s nock-miner \
+  "cd ~/nockchain && ./target/release/nockchain --mining-pubkey $PUBKEY --mine"
